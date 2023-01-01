1cd32de5b5e001adae0994dcbd9d1a1937352ff7
Ritesh Firodiya <ritesh.firodiya@gmail.com>
2023-01-01 23:30:20 +0530

    boiler repo completed

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+// TODO: use this store variable to create a store.
+
+// Note: you can delete the line below, but remember to create a new store variable
+export const store = {};
+
+// NOTE: Do not remove this code,its used for calculating your score, if removed it will give you zero marks
+if (window.Cypress) {
+  window.store = store;
+}
